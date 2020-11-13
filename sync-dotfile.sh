e6cca58a32d7862efa1702849d1b5c6ca301980d
k <k@pop-os.localdomain>
2020-11-13 17:48:53 -0500

    file exists

--- sync-dotfile.sh
+++ sync-dotfile.sh
@@ -9,12 +9,12 @@ fi
 
 while getopts 'i:e:' opt; do
     case "${opt}" in
-	    i) fn=$(basename i$OPTARG)
-	       cp $OPTARG $SRC_ROOT/dotfiles/$fn ;;
-        e) if [ -z "~/$OPTARG" ] ; then
-               echo "Copying ~/$OPTARG to ~/${OPTARG}.df_bak"
-               cp ~/$OPTARG ~/${OPTARG}.bak
-           fi
+            i) fn=$(basename i$OPTARG)
+                cp $OPTARG $SRC_ROOT/dotfiles/$fn ;;
+            e) if [ -f ~/$OPTARG ] ; then
+                   echo "Copying ~/$OPTARG to ~/${OPTARG}.df_bak"
+                   cp ~/$OPTARG ~/${OPTARG}.bak
+               fi
            cp $SRC_ROOT/dotfiles/$OPTARG ~/ ;;
         *) echo "Required params either -i or -e" && exit 1 ;;
     esac
